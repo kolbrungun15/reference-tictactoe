0128ab0a312c1645ee9f1aa7395b5254e339c738
Kolbrún <kolbrungun15@ru.is>
2016-12-16 15:48:13 +0000

    einu sinni enn

--- deployscript.sh
+++ deployscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/docker-compose.yml ec2-user@35.164.247.194:docker-compose.yml
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/docker-compose.yml ec2-user@35.164.247.194:docker-compose.yml
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/.env ec2-user@35.164.247.194:.env
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/.env ec2-user@35.164.247.194:.env
 
-ssh -i "/var/lib/jenkins/my-ec2-key-pair.pem" ec2-user@35.164.247.194 sudo docker-compose up -d
+sudo ssh -i "/var/lib/jenkins/my-ec2-key-pair.pem" ec2-user@35.164.247.194 docker-compose up -d
