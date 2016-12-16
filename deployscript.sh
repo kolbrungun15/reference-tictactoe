ec2da9f4c8fa98e49dee19063963f55baf2aacd9
Kolbrún <kolbrungun15@ru.is>
2016-12-16 16:49:54 +0000

    laga deployscript.sh

--- deployscript.sh
+++ deployscript.sh
@@ -2,6 +2,6 @@
 
 sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/docker-compose.yml ec2-user@35.164.247.194:docker-compose.yml
 
-sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/.env ec2-user@35.164.247.194:.env
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/my-ec2-key-pair.pem" /var/lib/jenkins/workspace/tictactoe/build.env ec2-user@35.164.247.194:.env
 
 ssh -i "/var/lib/jenkins/my-ec2-key-pair.pem" ec2-user@35.164.247.194 docker-compose up -d
